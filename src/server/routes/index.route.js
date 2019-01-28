65953112657d4871a22ba84b079b3334a9b0e14f
BMD <joey@bmdiot.com>
2019-01-28 18:24:01 +0800

    readme update

--- src/server/routes/index.route.js
+++ src/server/routes/index.route.js
@@ -1,12 +1,34 @@
-/* index.route.js */
 import express from 'express'
+import mysql from 'mysql'
+
 import config from '../../config/config'
 
 const router = express.Router()
 
+
 /* GET localhost:[port]/api page. */
 router.get('/', (req, res) => {
   res.send(`此路徑是: localhost:${config.port}/api`)
+})
+
+/* mysql連線測試 */
+router.get('/sqlTest', (req, res) => {
+  const connectionPool = mysql.createPool({ // 建立一個連線池
+    connectionLimit: 10, // 限制池子連線人數
+    host: config.mysqlHost, // 主機名稱
+    user: config.mysqlUserName, // 用戶名稱
+    password: config.mysqlPass, // 資料庫密碼
+    database: config.mysqlDatabase // 資料庫名稱
+  })
+  connectionPool.getConnection((err, connection) => { // 建立一個連線若錯誤回傳err
+    if (err) {
+      res.send(err)
+      console.log('連線失敗！')
+    } else {
+      res.send('連線成功！')
+      console.log(connection)
+    }
+  })
 })
 
 export default router
